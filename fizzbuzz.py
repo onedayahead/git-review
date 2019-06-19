12e313124ad4f1aede68711334d372ed32da0807
George Li <onedayahead19@gmail.com>
2019-06-19 09:57:08 -0700

    Basic fizzbuzz I/O loop

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,21 @@
 # This is where you will write your awesome calculator.
+
+def calc_fizzbuzz(n):
+    if n % 15 == 0:
+        return 'FizzBuzz'
+    elif n % 5 == 0:
+        return 'Buzz'
+    elif n % 3 == 0:
+        return 'Fizz'
+    else:
+        return str(n)
+
+
+def loop():
+    num = input('Enter a number: ')
+    print(calc_fizzbuzz(int(num)))
+
+
+if __name__ == '__main__':
+    while True:
+        loop()
